2a7c30b0218632ac5d651a706420a80f31a28afa
[Jonathan <[jonnyzgg@gmail.com]>
2024-12-05 01:33:48 -0500

    adjusted genres

--- src/Views/MoviesView.jsx
+++ src/Views/MoviesView.jsx
@@ -6,13 +6,13 @@ import "./MoviesView.css";
 
 function MoviesView() {
     const genres = [
-        { genre: "Science Fiction", id: 878 },
+        { genre: "Sci-Fi", id: 878 },
         { genre: "Thriller", id: 53 },
         { genre: "Adventure", id: 12 },
         { genre: "Family", id: 10751 },
         { genre: "Animation", id: 16 },
         { genre: "Action", id: 28 },
-        { genre: "Drama", id: 18 },
+        { genre: "History", id: 36 },
         { genre: "Fantasy", id: 14 },
         { genre: "Horror", id: 27 },
         { genre: "Comedy", id: 35 }
